8c1a1ffe59ee79fe1ce1ddd484b3f9eb53d89013
Nicolas Herbaut <nherbaut@labri.fr>
2017-11-23 10:59:15 +0100

    placement?.

--- vagrant/bootstrap_master.sh
+++ vagrant/bootstrap_master.sh
@@ -5,6 +5,7 @@ echo "$1 salt" >> /etc/hosts
 apt-get update
 apt-get install wget apt-transport-https --yes
 wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+rm -rf /etc/apt/sources.list.d/saltstack.list || true
 echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" >> /etc/apt/sources.list.d/saltstack.list
 apt-get update
 apt-get install salt-master salt-minion python-pygit2 python-git --yes
