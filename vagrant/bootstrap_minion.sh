a9ede1c31c9c1a81d2ca128be116349795440e89
Nicolas Herbaut <nherbaut@labri.fr>
2017-11-23 10:54:57 +0100

    fixed bootstrap

--- vagrant/bootstrap_minion.sh
+++ vagrant/bootstrap_minion.sh
@@ -3,7 +3,7 @@
 
 echo "$1 salt" >> /etc/hosts
 apt-get update
-apt-get install wget
+apt-get install wget apt-transport-https --yes
 wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" >> /etc/apt/sources.list.d/saltstack.list
 apt-get update && apt-get install salt-minion python-pygit2 python-git --yes
